c135fc9661cf21dfb824621f94185dd8853ee662
Israel Pontes <71298288+Israelpsilva8246@users.noreply.github.com>
2022-04-25 20:16:02 -0300

    Método POST

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -5,12 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L, "Attack on titan"), new Anime(2L, "Naruto"));
+    private static List<Anime> animes;
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Attack on titan"), new Anime(2L, "Naruto")));
+    }
 
     //private final AnimeRepository animeRepository;
     public List<Anime> listAll(){
@@ -22,5 +27,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId() == id)
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 10000));
+        animes.add(anime);
+        return anime;
     }
 }
