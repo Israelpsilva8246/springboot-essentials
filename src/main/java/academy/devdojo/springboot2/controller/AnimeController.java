af4a818f98ca42b6ce142d3a0accf8c9d7ec0d1f
Israel Pontes <israel.pontes.1920@gmail.com>
2022-05-01 10:41:06 -0300

    Unit Tests pt 01

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -4,7 +4,6 @@ import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import academy.devdojo.springboot2.service.AnimeService;
-import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -14,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -22,19 +20,15 @@ import java.util.List;
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
-
-    private final DateUtil dateUtil;
     private final AnimeService animeService;
 
     //localhost:8080/anime/list
     @GetMapping
     public ResponseEntity<Page<Anime>> list(Pageable pageable){
-        log.info(dateUtil.formartLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(animeService.listAll(pageable), HttpStatus.OK);
     }
    @GetMapping(path = "/all")
     public ResponseEntity<List<Anime>> listAll(){
-        log.info(dateUtil.formartLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(animeService.listAllNonPageable(), HttpStatus.OK);
     }
 
--- src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
+++ src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.AnimeCreator;
 import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -25,24 +26,24 @@ class AnimeRepositoryTest {
     @Test
     @DisplayName("Save persists anime when successful")
     void save_PersistAnime_WhenSuccessful(){
-        Anime animeToBeSaved = createAnime();
+        Anime createAnimeToBeSaved = AnimeCreator.createAnimeToBeSaved();
 
-        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Anime animeSaved = this.animeRepository.save(createAnimeToBeSaved);
 
         Assertions.assertThat(animeSaved).isNotNull();
 
         Assertions.assertThat(animeSaved.getId()).isNotNull();
 
-        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(createAnimeToBeSaved.getName());
 
     }
 
     @Test
     @DisplayName("Save updates anime when successful")
     void save_UpdatesAnime_WhenSuccessful(){
-        Anime animeToBeSaved = createAnime();
+        Anime createAnimeToBeSaved = AnimeCreator.createAnimeToBeSaved();
 
-        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Anime animeSaved = this.animeRepository.save(createAnimeToBeSaved);
 
         animeSaved.setName("Overlord");
 
@@ -61,9 +62,9 @@ class AnimeRepositoryTest {
     @Test
     @DisplayName("Delete removes anime when successful")
     void delete_RemovesAnime_WhenSuccessful(){
-        Anime animeToBeSaved = createAnime();
+        Anime createAnimeToBeSaved = AnimeCreator.createAnimeToBeSaved();
 
-        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Anime animeSaved = this.animeRepository.save(createAnimeToBeSaved);
 
         this.animeRepository.delete(animeSaved);
 
@@ -75,9 +76,9 @@ class AnimeRepositoryTest {
     @Test
     @DisplayName("Find By Name return list of anime when successful")
     void findById_ReturnsListOfAnime_WhenSuccessful(){
-        Anime animeToBeSaved = createAnime();
+        Anime createAnimeToBeSaved = AnimeCreator.createAnimeToBeSaved();
 
-        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Anime animeSaved = this.animeRepository.save(createAnimeToBeSaved);
 
         String name = animeSaved.getName();
 
@@ -112,11 +113,7 @@ class AnimeRepositoryTest {
 
     }
 
-    private Anime createAnime(){
-        return Anime.builder()
-                .name("Hajime no ippo")
-                .build();
-    }
+
 
 
 }
