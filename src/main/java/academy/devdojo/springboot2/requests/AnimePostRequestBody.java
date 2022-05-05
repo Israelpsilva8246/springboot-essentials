0200271dabb2ba41a05403cf1a33903297186308
Israel Pontes <israel.pontes.1920@gmail.com>
2022-05-05 11:07:49 -0300

    Documentação com SpringDoc OpenAPI pt 02

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,18 +1,16 @@
 package academy.devdojo.springboot2.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
+    @Schema(description = "This is anime's name", example = "Tensei Shittara Slime Datta Ken")
     private String name;
 
-    @URL(message = "The URL is not valid")
-    private String url;
 }
